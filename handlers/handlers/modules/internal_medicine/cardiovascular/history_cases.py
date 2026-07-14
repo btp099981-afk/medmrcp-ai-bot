dff80a85fc566d7bee6db458e067620f87f7b97a
btp099981-afk <btp099981@gmail.com>
2026-07-14 08:49:55 +0200

    Update history_cases.py

--- handlers/handlers/modules/internal_medicine/cardiovascular/history_cases.py
+++ handlers/handlers/modules/internal_medicine/cardiovascular/history_cases.py
@@ -1,1 +1,20 @@
 
+chest_pain_case = {
+    "title": "Chest Pain",
+    "patient": {
+        "name": "Ahmed",
+        "age": 55,
+        "gender": "Male"
+    },
+    "complaint": "Chest pain",
+    "history": {
+        "onset": "Started 2 hours ago",
+        "site": "Central chest",
+        "character": "Pressure-like",
+        "radiation": "Left arm",
+        "associated": [
+            "Shortness of breath",
+            "Sweating"
+        ]
+    }
+}
