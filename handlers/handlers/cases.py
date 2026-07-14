04c67d5c14e99fa3d8749ed2bcc7983c453516f2
btp099981-afk <btp099981@gmail.com>
2026-07-14 19:15:57 +0200

    Update cases.py

--- handlers/handlers/cases.py
+++ handlers/handlers/cases.py
@@ -1,1 +1,13 @@
 
+from modules.internal_medicine.cardiovascular.history_cases import chest_pain_case
+
+
+def get_chest_pain_case():
+
+    return (
+        f"🧑 Patient: {chest_pain_case['patient']['name']}\n"
+        f"Age: {chest_pain_case['patient']['age']}\n\n"
+        f"Chief complaint:\n"
+        f"{chest_pain_case['complaint']}\n\n"
+        "Start taking the history."
+    )
